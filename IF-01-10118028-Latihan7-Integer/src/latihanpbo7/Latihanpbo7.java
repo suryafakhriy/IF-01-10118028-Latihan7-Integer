064c09487267f8fb3b7f8ee3c1cd80a149b68560
suryafakhriy <55834014+suryafakhriy@users.noreply.github.com>
2019-09-26 21:22:06 +0700

    Add files via upload

---
+++ IF-01-10118028-Latihan7-Integer/src/latihanpbo7/Latihanpbo7.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanpbo7;
+
+/**
+ *
+ * @author User
+ * NAMA      : Surya Fakhriy Hasbi
+   KELAS     : IF-01 2018
+   NIM       : 10118028
+   DESKRIPSI : Integer
+ */
+public class Latihanpbo7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+    }
+    
+}
